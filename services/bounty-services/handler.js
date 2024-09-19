622f7f2d2f28ed92da7270a502fd38cf0b8b0518
Abhishek.Diwate <abhishek.diwate@ivycomptech.com>
2024-09-19 17:21:19 +0530

    route-setup

--- services/bounty-services/handler.js
+++ services/bounty-services/handler.js
@@ -1,7 +1,10 @@
 const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
+const bountyRoute = require("./bountyRoute");
 
-
-
+app.get("/", (req, res) => {
+    res.send("Hello World From Bounty Services");
+});
+app.use(process.env.BOUNTY_SERVICE_URL, bountyRoute);
 exports.handler = serverless(app);
--- services/tags-services/handler.js
+++ services/tags-services/handler.js
@@ -1,7 +1,10 @@
 const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
+const tagsRoute = require("./tagsRoute");
 
-
-
+app.get("/", (req, res) => {
+    res.send("Hello World From Tags Services");
+});
+app.use(process.env.TAGS_SERVICE_URL, tagsRoute);
 exports.handler = serverless(app);
